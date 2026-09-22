81b5d7efe264afed25afa425f2d191537b101f7a
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-22 20:57:59 -0300

    feat: publish App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,36 @@
+import { Header } from '../components/layout/Header';
+import { Footer } from '../components/layout/Footer';
+import { HeroCountdown } from '../components/sections/HeroCountdown';
+import { DashboardMetrics } from '../components/sections/DashboardMetrics';
+import { ElectoralProfile } from '../components/sections/ElectoralProfile';
+import { TransportCalculator } from '../components/TransportCalculator';
+import { SanitationHealthSection } from '../components/sections/SanitationHealthSection';
+import { PoliticalRadar } from '../components/sections/PoliticalRadar';
+import { PoliticalResearch } from '../components/sections/PoliticalResearch';
+import { Central2026 } from '../components/sections/Central2026';
+import { DataQualityPanel } from '../components/sections/DataQualityPanel';
+import { BudgetSection } from '../components/sections/BudgetSection';
+import { DataExportActions } from '../components/DataExportActions';
+import { EvidenceMap } from '../components/sections/EvidenceMap';
+import { ThemeProvider } from '../context/ThemeContext';
+
+export function App() {
+  return <ThemeProvider>
+    <Header />
+    <HeroCountdown />
+    <main id="main-content">
+      <DashboardMetrics />
+      <ElectoralProfile />
+      <TransportCalculator />
+      <SanitationHealthSection />
+      <PoliticalRadar />
+      <PoliticalResearch />
+      <BudgetSection />
+      <Central2026 />
+      <DataQualityPanel />
+      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6"><DataExportActions /></div>
+      <EvidenceMap />
+    </main>
+    <Footer />
+  </ThemeProvider>;
+}
