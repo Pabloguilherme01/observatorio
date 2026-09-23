191897ff717482a7d88276c35be16432b2b41a6c
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 13:40:42 -0300

    perf: defer below-fold section group src/components/sections/DeferredElectionGroup.tsx

---
+++ src/components/sections/DeferredElectionGroup.tsx
@@ -0,0 +1,11 @@
+import { BudgetSection } from './BudgetSection';
+import { BudgetImpact } from './BudgetImpact';
+import { Central2026 } from './Central2026';
+
+export default function DeferredElectionGroup() {
+  return <>
+    <BudgetSection />
+    <BudgetImpact />
+    <Central2026 />
+  </>;
+}
