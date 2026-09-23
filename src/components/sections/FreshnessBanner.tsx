956ba9ec9f119a2c7bdb7a789412243f2dd4889d
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 03:17:32 -0300

    chore: align V35 freshness storage

--- src/components/sections/FreshnessBanner.tsx
+++ src/components/sections/FreshnessBanner.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { observatorioData as d } from '../../data/observatorioData';
 import { formatDate } from '../../utils/formatters';
 
-const VISIT_KEY = 'observatorio-last-visit-v34';
+const VISIT_KEY = 'observatorio-last-visit-v35';
 
 export function FreshnessBanner() {
   const [previousVisit, setPreviousVisit] = useState<string | null>(null);
