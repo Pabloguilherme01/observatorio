d53caf8b8763e0a4aa959aecc2091eeac2608952
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 13:40:36 -0300

    perf: defer below-fold section group src/components/sections/DeferredContextGroup.tsx

---
+++ src/components/sections/DeferredContextGroup.tsx
@@ -0,0 +1,23 @@
+import { FreshnessBanner } from './FreshnessBanner';
+import { SnapshotChanges } from './SnapshotChanges';
+import { ResultsLiveBanner } from './ResultsLiveBanner';
+import { ContextComparison } from './ContextComparison';
+import { ElectoralProfile } from './ElectoralProfile';
+import { DemographicDynamic } from './DemographicDynamic';
+import { TransportCalculator } from '../TransportCalculator';
+import { DataInsights } from './DataInsights';
+import { SanitationHealthSection } from './SanitationHealthSection';
+
+export default function DeferredContextGroup() {
+  return <>
+    <FreshnessBanner />
+    <SnapshotChanges />
+    <ResultsLiveBanner />
+    <ContextComparison />
+    <ElectoralProfile />
+    <DemographicDynamic />
+    <TransportCalculator />
+    <DataInsights />
+    <SanitationHealthSection />
+  </>;
+}
