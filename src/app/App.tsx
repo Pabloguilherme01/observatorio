eb2f870ca00908feb6a286574b4a163692cc2b1d
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-22 23:16:22 -0300

    feat: improve page navigation and recovery controls

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/layout/Header';
 import { Footer } from '../components/layout/Footer';
+import { ScrollTopButton } from '../components/layout/ScrollTopButton';
 import { HeroCountdown } from '../components/sections/HeroCountdown';
 import { DashboardMetrics } from '../components/sections/DashboardMetrics';
 import { ElectoralProfile } from '../components/sections/ElectoralProfile';
@@ -15,22 +16,27 @@ import { EvidenceMap } from '../components/sections/EvidenceMap';
 import { ThemeProvider } from '../context/ThemeContext';
 
 export function App() {
-  return <ThemeProvider>
-    <Header />
-    <HeroCountdown />
-    <main id="main-content">
-      <DashboardMetrics />
-      <ElectoralProfile />
-      <TransportCalculator />
-      <SanitationHealthSection />
-      <PoliticalRadar />
-      <PoliticalResearch />
-      <BudgetSection />
-      <Central2026 />
-      <DataQualityPanel />
-      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6"><DataExportActions /></div>
-      <EvidenceMap />
-    </main>
-    <Footer />
-  </ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <Header />
+      <HeroCountdown />
+      <main id="main-content">
+        <DashboardMetrics />
+        <ElectoralProfile />
+        <TransportCalculator />
+        <SanitationHealthSection />
+        <PoliticalRadar />
+        <PoliticalResearch />
+        <BudgetSection />
+        <Central2026 />
+        <DataQualityPanel />
+        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6">
+          <DataExportActions />
+        </div>
+        <EvidenceMap />
+      </main>
+      <Footer />
+      <ScrollTopButton />
+    </ThemeProvider>
+  );
 }
