fdde7aa98f97ef12239df1741ee6f164508a49b5
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 06:34:41 -0300

    social: align campaign links with V43

--- src/components/InstagramSyncHub.tsx
+++ src/components/InstagramSyncHub.tsx
@@ -19,7 +19,7 @@ function trackedUrl(anchor: string) {
   const url = new URL(window.location.origin + window.location.pathname);
   url.searchParams.set('utm_source', 'instagram');
   url.searchParams.set('utm_medium', 'organic');
-  url.searchParams.set('utm_campaign', 'observatorio-v40');
+  url.searchParams.set('utm_campaign', 'observatorio-v43');
   url.hash = anchor;
   return url.toString();
 }
