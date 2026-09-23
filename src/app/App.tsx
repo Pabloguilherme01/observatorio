89845fed309ab430f8d4ac3f90ea68f8d241354a
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 05:52:11 -0300

    feat: mount civic data literacy quiz

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -31,6 +31,7 @@ import { ContextComparison } from '../components/sections/ContextComparison';
 import { CivicActionHub } from '../components/sections/CivicActionHub';
 import { LanguageModeProvider } from '../context/LanguageModeContext';
 import { AudienceHub } from '../components/AudienceHub';
+import { QuickQuiz } from '../components/sections/QuickQuiz';
 import { InstagramSyncHub } from '../components/InstagramSyncHub';
 import { ProjectTrustPanel } from '../components/ProjectTrustPanel';
 
@@ -43,6 +44,7 @@ export function App() {
           <HeroCountdown />
           <main id="main-content">
             <AudienceHub />
+            <QuickQuiz />
             <ProjectTrustPanel />
             <ExecutiveSummary />
             <FreshnessBanner />
