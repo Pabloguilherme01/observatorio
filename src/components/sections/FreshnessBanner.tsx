a4aec8216f2238f3c62aba8e540c69c5875783d6
Pabloguilherme01 <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-23 02:20:47 -0300

    chore: rotate freshness storage key to V30

--- src/components/sections/FreshnessBanner.tsx
+++ src/components/sections/FreshnessBanner.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { observatorioData as d } from '../../data/observatorioData';
 import { formatDate } from '../../utils/formatters';
 
-const VISIT_KEY = 'observatorio-last-visit-v29';
+const VISIT_KEY = 'observatorio-last-visit-v30';
 
 export function FreshnessBanner() {
   const [previousVisit, setPreviousVisit] = useState<string | null>(null);
