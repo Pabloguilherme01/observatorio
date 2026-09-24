1a865e5044b20afc04d5a41d7ffcc73ae8a097e9
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 04:06:28 -0300

    refactor: organiza carregamento do grupo civico

--- src/components/sections/DeferredCivicGroup.tsx
+++ src/components/sections/DeferredCivicGroup.tsx
@@ -4,12 +4,20 @@ import { ElectionTimeline } from './ElectionTimeline';
 import { Electoral360 } from './Electoral360';
 import { CivicActionHub } from './CivicActionHub';
 
+const civicModules = [
+  PoliticalRadar,
+  PoliticalResearch,
+  ElectionTimeline,
+  Electoral360,
+  CivicActionHub,
+];
+
 export default function DeferredCivicGroup() {
-  return <>
-    <PoliticalRadar />
-    <PoliticalResearch />
-    <ElectionTimeline />
-    <Electoral360 />
-    <CivicActionHub />
-  </>;
+  return (
+    <>
+      {civicModules.map((Module) => (
+        <Module key={Module.name} />
+      ))}
+    </>
+  );
 }
