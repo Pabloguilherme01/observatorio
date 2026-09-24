df8277eeb14c8944518b72a29389bf92e4ffe214
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 14:14:20 -0300

    refactor: consolidar grupos públicos

--- src/components/sections/DeferredTrustGroup.tsx
+++ src/components/sections/DeferredTrustGroup.tsx
@@ -1,9 +1,5 @@
 import { ProjectTrustPanel } from '../ProjectTrustPanel';
-import { QuickQuiz } from './QuickQuiz';
 
 export default function DeferredTrustGroup() {
-  return <>
-    <ProjectTrustPanel />
-    <QuickQuiz />
-  </>;
+  return <ProjectTrustPanel />;
 }
