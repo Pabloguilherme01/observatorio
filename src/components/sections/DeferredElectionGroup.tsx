ee415356a55e794faca6fbc80e28074ebeeabef0
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 04:10:31 -0300

    refactor: simplify deferred election section rendering

--- src/components/sections/DeferredElectionGroup.tsx
+++ src/components/sections/DeferredElectionGroup.tsx
@@ -2,12 +2,16 @@ import { BudgetSection } from './BudgetSection';
 import { BudgetImpact } from './BudgetImpact';
 import { Central2026 } from './Central2026';
 
-export default function DeferredElectionGroup() {
-  const sections = [BudgetSection, BudgetImpact, Central2026];
+const electionSections = [
+  BudgetSection,
+  BudgetImpact,
+  Central2026,
+];
 
+export default function DeferredElectionGroup() {
   return (
     <>
-      {sections.map((Section) => (
+      {electionSections.map((Section) => (
         <Section key={Section.name} />
       ))}
     </>
