466f94126fb2020c1e681cd7c4f358d059ff63d0
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 04:04:36 -0300

    refactor: organiza carregamento do grupo eleitoral

--- src/components/sections/DeferredElectionGroup.tsx
+++ src/components/sections/DeferredElectionGroup.tsx
@@ -3,9 +3,13 @@ import { BudgetImpact } from './BudgetImpact';
 import { Central2026 } from './Central2026';
 
 export default function DeferredElectionGroup() {
-  return <>
-    <BudgetSection />
-    <BudgetImpact />
-    <Central2026 />
-  </>;
+  const sections = [BudgetSection, BudgetImpact, Central2026];
+
+  return (
+    <>
+      {sections.map((Section) => (
+        <Section key={Section.name} />
+      ))}
+    </>
+  );
 }
