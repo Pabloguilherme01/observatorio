2e39838364d00e6dd592d6dc284cfcd89b371379
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 09:58:30 -0300

    feat: integrar painel eleitoral ao fluxo cívico

--- src/components/sections/DeferredCivicGroup.tsx
+++ src/components/sections/DeferredCivicGroup.tsx
@@ -3,11 +3,13 @@ import { PoliticalResearch } from './PoliticalResearch';
 import { ElectionTimeline } from './ElectionTimeline';
 import { Electoral360 } from './Electoral360';
 import { CivicActionHub } from './CivicActionHub';
+import { ElectoralOverview } from './ElectoralOverview';
 
 export default function DeferredCivicGroup() {
   return <>
     <PoliticalRadar />
     <PoliticalResearch />
+    <ElectoralOverview />
     <ElectionTimeline />
     <Electoral360 />
     <CivicActionHub />
