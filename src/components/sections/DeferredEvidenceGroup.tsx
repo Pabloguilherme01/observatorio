bfa402acf228e8762c173bbc0d09f3ea18ce87ba
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-24 04:06:56 -0300

    refactor: organiza grupo de evidencias para manutencao

--- src/components/sections/DeferredEvidenceGroup.tsx
+++ src/components/sections/DeferredEvidenceGroup.tsx
@@ -4,12 +4,18 @@ import { DataExportActions } from '../DataExportActions';
 import { EvidenceMap } from './EvidenceMap';
 import { Footer } from '../layout/Footer';
 
+const evidenceModules = [
+  DataQualityPanel,
+  EvidenceChain,
+  EvidenceMap,
+];
+
 export default function DeferredEvidenceGroup() {
   return <>
-    <DataQualityPanel />
-    <EvidenceChain />
-    <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6"><DataExportActions /></div>
-    <EvidenceMap />
+    {evidenceModules.map((Module, index) => <Module key={index} />)}
+    <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6">
+      <DataExportActions />
+    </div>
     <Footer />
   </>;
 }
