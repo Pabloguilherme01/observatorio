4295135f7f73bf1b018a943ff156ee2cfa68d96d
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-25 11:24:31 -0300

    refactor: consolidate electoral research section

--- src/components/sections/DeferredCivicGroup.tsx
+++ src/components/sections/DeferredCivicGroup.tsx
@@ -1,4 +1,3 @@
-import { PoliticalRadar } from './PoliticalRadar';
 import { PoliticalResearch } from './PoliticalResearch';
 import { ElectionTimeline } from './ElectionTimeline';
 import { Electoral360 } from './Electoral360';
@@ -6,7 +5,6 @@ import { CivicActionHub } from './CivicActionHub';
 
 export default function DeferredCivicGroup() {
   return <>
-    <PoliticalRadar />
     <PoliticalResearch />
     <ElectionTimeline />
     <Electoral360 />
