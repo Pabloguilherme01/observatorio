8032d96829427ed3f5c08bb2f5f8a69d5b94104c
PGTECH.IA <194545453+Pabloguilherme01@users.noreply.github.com>
2026-09-25 16:55:40 -0300

    ux: keep civic home focused and defer technical election detail

--- src/components/sections/DeferredCivicGroup.tsx
+++ src/components/sections/DeferredCivicGroup.tsx
@@ -1,13 +1,17 @@
+import { useLanguageMode } from '../../context/LanguageModeContext';
 import { PoliticalResearch } from './PoliticalResearch';
 import { ElectionTimeline } from './ElectionTimeline';
 import { Electoral360 } from './Electoral360';
 import { CivicActionHub } from './CivicActionHub';
 
 export default function DeferredCivicGroup() {
+  const { mode } = useLanguageMode();
+  const technical = mode === 'technical';
+
   return <>
-    <PoliticalResearch />
     <ElectionTimeline />
-    <Electoral360 />
     <CivicActionHub />
+    {technical && <PoliticalResearch />}
+    {technical && <Electoral360 />}
   </>;
 }
